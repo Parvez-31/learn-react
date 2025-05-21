eb9eed9e96b71c76a3df5501f6f032611c736526
Parvez-31 <parveztarafder31@gmail.com>
2025-05-21 15:57:02 +0530

    a component's memory

--- module-2/src/App.jsx
+++ module-2/src/App.jsx
@@ -1,14 +1,36 @@
-import Toolbar from "./components/Toolbar";
+// import { sculptureList } from "./data.js";
+import { useState } from "react";
+import { sculptureList } from "./utils/data";
 
-const App = () => {
+export default function App() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
     <>
-      <Toolbar
-        onPlayMovie={() => alert("play moive")}
-        onUpladMoive={() => alert("upload movie")}
-      />
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
     </>
   );
-};
-
-export default App;
+}
