1c2939fdccf713d4a1c1dd91a24a500634510e0b
Parvez-31 <parveztarafder31@gmail.com>
2025-06-07 06:53:54 +0530

    chef-claude

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import chefClaudeLogo from "/chef-logo.png";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <img src={chefClaudeLogo} />
+        <h1>Chef Claude</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
