f1eb78bddd3f9767e4aa53afd0b5cacf91a8dd13
Parvez-31 <parveztarafder31@gmail.com>
2025-06-14 06:51:43 +0530

    finished meme-generator project

---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="https://pngfre.com/wp-content/uploads/troll-face-78-1-300x300.png" />
+      <h1>Meme Generator</h1>
+    </header>
+  );
+}
---
+++ meme-generator/src/components/Main.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+
+export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  /**
+   * Challenge: Get a random image from the array of
+   * allMemes when the user clicks the button. Once
+   * you've gotten a random image from the array, make
+   * sure to write the code that will display that
+   * random meme image to the page.
+   */
+  const handleButton = () => {
+    const randomIndex = Math.floor(Math.random() * allMemes.length);
+    const randomMeme = allMemes[randomIndex]; // I get 1 meme object from here.
+
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        imageUrl: randomMeme.url,
+      };
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+        </label>
+        <button onClick={handleButton}>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
+}
